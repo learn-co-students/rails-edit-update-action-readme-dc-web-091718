1dcc8ffc9e83963b03c94b71f5ebb84dafbcc8e4
Jacqueline Chenault <adigitalnative@gmail.com>
2018-10-12 16:40:48 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,11 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update]
+
   def index
     @posts = Post.all
   end
 
   def show
-    @post = Post.find(params[:id])
   end
 
   def new
@@ -19,5 +20,12 @@ class PostsController < ApplicationController
     redirect_to post_path(@post)
   end
 
-  # add edit and update methods here
+  def edit
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
 end
